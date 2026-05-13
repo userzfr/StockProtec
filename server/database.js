b8e127298a1e6eefa4a46d230c215c0368e92418
Userzfr <mathieumerle42@gmail.com>
2026-05-14 00:16:19 +0200

    Mise à jour de la dépendance better-sqlite3 vers la version 12.10.0 et ajout de l'importation de hashPassword dans database.js

--- server/database.js
+++ server/database.js
@@ -1,6 +1,7 @@
 import Database from 'better-sqlite3';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { hashPassword } from './password.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
